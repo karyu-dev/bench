c87e6ac3897d0cc95fda3dd1504b97001f982c55
val <valentin.gourdonneau@gmail.com>
2026-08-13 10:01:15 +0000

    ajout bench disk

--- src/main.py
+++ src/main.py
@@ -9,32 +9,32 @@ import runs
 from disk_bench import disk_benchmark_run
 
 
-GB = (1024 * 1024 * 1024)
+# GB = (1024 * 1024 * 1024)
 
-info = get_cpu_info()
-# print (info)
+# info = get_cpu_info()
+# # print (info)
 
-sysinfo = SysInfo(
-    info["brand_raw"],
-    cpu_count(),
-    round((virtual_memory().available / GB), 2),
-    platform.platform(),
-    info["l1_data_cache_size"],
-    info["l1_instruction_cache_size"],
-    info["l2_cache_size"],
-    info["l3_cache_size"] # BUG SUR AMD, A CHANGER PAR UNE LECTURE DE /sys/devices/system/cpu/cpu0/cache/index3/size
-)
+# sysinfo = SysInfo(
+#     info["brand_raw"],
+#     cpu_count(),
+#     round((virtual_memory().available / GB), 2),
+#     platform.platform(),
+#     info["l1_data_cache_size"],
+#     info["l1_instruction_cache_size"],
+#     info["l2_cache_size"],
+#     info["l3_cache_size"] # BUG SUR AMD, A CHANGER PAR UNE LECTURE DE /sys/devices/system/cpu/cpu0/cache/index3/size
+# )
 
 
 
-print(sysinfo.cpu_model)
-print(sysinfo.num_threads)
-print(sysinfo.available_memory)
-print(sysinfo.kernel)
-print(sysinfo.l1_data_size)
-print(sysinfo.l1_instr_size)
-print(sysinfo.l2_size)
-print(sysinfo.l3_size)
+# print(sysinfo.cpu_model)
+# print(sysinfo.num_threads)
+# print(sysinfo.available_memory)
+# print(sysinfo.kernel)
+# print(sysinfo.l1_data_size)
+# print(sysinfo.l1_instr_size)
+# print(sysinfo.l2_size)
+# print(sysinfo.l3_size)
 
 
 
